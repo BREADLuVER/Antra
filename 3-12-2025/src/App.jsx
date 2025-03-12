6c3b6a5ac6269cb8f117561581859b5428e41d3b
BREADLuVER <ry2050@nyu.edu>
2025-03-12 12:02:32 -0400

    todo added

--- 3-12-2025/src/App.jsx
+++ 3-12-2025/src/App.jsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./styles.css"
+import { useState, useEffect, useRef } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem('todos')
+    return savedTodos ? JSON.parse(savedTodos) : []
+  })
 
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
+
+  const inputRef = useRef(null)
+  function addTodo(e) {
+    e.preventDefault()
+    if (newItem.trim() === "") return
+
+    setTodos((currentTodos) => {
+      return [...currentTodos, 
+        { id:crypto.randomUUID(), title:newItem, completed:false},]
+    })
+
+    setNewItem("")
+    inputRef.current?.focus()
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo=>{
+        if(todo.id === id) {
+          return {...todo, completed}
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={addTodo} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input
+            ref={inputRef}
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+          />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+
+      <h1 className="header">To Do</h1>
+      <ul className="list">
+        {todos.length===0 && "No Todos"}
+        {todos.map(todo => {
+          return(
+            <li key={todo.id}>
+            <label>
+              <input type="checkbox" checked={todo.completed} 
+              onChange={e => toggleTodo(todo.id, e.target.checked)}/>
+              {todo.title}
+            </label>
+            <button onClick={() => deleteTodo(todo.id)} className="btn">Delete</button>
+          </li>
+          )
+        })}
+      </ul>
     </>
   )
 }
-
-export default App
